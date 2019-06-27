5927f33345912b9f9dd8962275a5d8ba99cad062
Stanley Shyiko <stanley.shyiko@gmail.com>
2019-06-26 21:05:32 -0700

    Strip the data

--- supervisor_stdout.py
+++ supervisor_stdout.py
@@ -5,14 +5,10 @@ def write_stdout(s):
     sys.stdout.write(s)
     sys.stdout.flush()
 
-def write_stderr(s):
-    sys.stderr.write(s)
-    sys.stderr.flush()
-
 def main():
     while 1:
         write_stdout('READY\n') # transition from ACKNOWLEDGED to READY
-        line = sys.stdin.readline()  # read header line from stdin
+        line = sys.stdin.readline() # read header line from stdin
         headers = dict([ x.split(':') for x in line.split() ])
         data = sys.stdin.read(int(headers['len'])) # read the event payload
         write_stdout('RESULT %s\n%s'%(len(data), data)) # transition from READY to ACKNOWLEDGED
@@ -20,9 +16,10 @@ def main():
 def event_handler(event, response):
     line, data = response.split('\n', 1)
     headers = dict([ x.split(':') for x in line.split() ])
-    lines = data.split('\n')
-    prefix = '%s %s | '%(headers['processname'], headers['channel'])
-    print('\n'.join([ prefix + l for l in lines ]))
+    lines = data.strip().split('\n')
+    prefix = '%s: '%(headers['processname'])
+    print('\n'.join([ prefix + l for l in lines]),
+        file=sys.stdout if headers['channel'] == 'stdout' else sys.stderr)
 
 if __name__ == '__main__':
     main()
